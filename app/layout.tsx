6fac461d827560549117b42612ac24875d44b1b9
mmk4-81 <mmkkarbalaei@gmail.com>
2025-01-13 23:49:00 +0330

    Added Meeting Room functionality with join meeting feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
